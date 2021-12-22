aa06de2481ea6cfdc82fc8ab810501d0d2b1fc61
npduval <nickman92186@yahoo.com>
2021-12-21 20:45:46 -0500

    added additional route files

---
+++ Develop/controllers/api/index.js
@@ -0,0 +1,10 @@
+//boilerplate code
+
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+
+
+router.use('/users', userRoutes);
+router.use('/projects', projectRoutes);
+
+module.exports = router;
