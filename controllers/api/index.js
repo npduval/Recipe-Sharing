74eb054f04ffd0ae35009bb7e3d1124891492bee
Nick Duval <nicholaspduval74@gmail.com>
2022-01-04 20:16:59 -0500

    rearragned file structure and built out api/html routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,10 @@
 
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const projectRoutes = require('./recipeRoutes');
 
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/recipes', recipeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,13 +11,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'secret',
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -26,8 +25,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
