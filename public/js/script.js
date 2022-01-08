d65107e058c51ddf709de01713f3c4ee08dabb03
Robert Augusta <RobAAugusta@gmail.com>
2022-01-08 10:07:59 -0500

    Added some more functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,68 @@
-var modal = document.getElementById('id01');
+var countries = ["Apple Crisp", "Applesauce", "Beef Brisket", "Beef Stew", "Candied Pecans", "Carne Asada Tacos", "Chicken Pineapple Quesadilla", "Chocolate Chocolate Chip Cookies", "Crispy Curry Chicken", "Crispy Fish Tacos", "Firecracker Meatballs", "Garlic Roasted Potatoes", "German Sweet Chocolate Cake", "Guacamole Dip", "Gyros (With Homemade Tzatziki Sauce)", "Hard Cider", "Key Lime Pie", "Lemon Meringue Pie", "Mexican Street Corn", "Mexican-Style Patty Melt", "Punch", "Red Sangria", "Schnitzel Sandwich", "Seared Steaks & Aoili", "Sheet Pan BBQ Pork", "Spiced Chicken & Kale Salad", "Tomato Sauce", "Whiskey Sour", "White Peach Sangria", "Zucchini Quesadillas"];
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function autocomplete(inp, arr) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(a);
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+              b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
   }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
 }
