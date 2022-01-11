fccec35890f830d85d4aa61152b8152371ec4d57
Nick Duval <nicholaspduval74@gmail.com>
2022-01-11 17:39:40 -0500

    search

--- public/js/search.js
+++ public/js/search.js
@@ -2,17 +2,36 @@
 
 
 
-const searchfunction= async () =>{
+const searchfunction= async (event) =>{
 
 const search = document.getElementById("search").value.trim();
+const container = document.getElementById("container");
+
+event.preventDefault();
 
 if (search) {
   
   const response = await fetch('/api/recipes/search', {
-    method: 'GET',
+    method: 'POST',
     body: JSON.stringify({ search }),
     headers: { 'Content-Type': 'application/json' },
   });
+
+  const data = await response.json();
+
+  container.textContent = '';
+
+  const ul = document.createElement('ul');
+
+  data.forEach(element => {
+    const li = document.createElement('li');
+
+    li.textContent = element.name;
+
+    ul.appendChild(li);
+  });
+
+  container.appendChild(ul);
 
 }  else {
   alert('Please enter a valid search');
